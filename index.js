0521e11e4e61c45f5a4fc4e1843ba9b6e305da0d
RuoyuWang <klaus1201810802@gmail.com>
2020-07-27 22:10:50 +0100

    built macos dock and topbar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Handle Theme
+
 const themeDots = document.querySelectorAll('.theme-dot');
 
 themeDots.forEach(i => {
@@ -32,3 +34,66 @@ if (!theme) {
 } else {
   setTheme(theme);
 }
+
+// Handle apple window dots
+const dots = document.querySelectorAll('.dot');
+dots.forEach(dot =>
+  dot.addEventListener('mouseover', function () {
+    document.querySelectorAll('.dot > i').forEach(i => (i.style.display = 'block'));
+  })
+);
+dots.forEach(dot =>
+  dot.addEventListener('click', function () {
+    const name = this.getAttribute('name');
+    switch (name) {
+      case 'close':
+        console.log('close');
+        return;
+      case 'zoomout':
+        console.log('zoomout');
+        return;
+      case 'zoomin':
+        console.log('zoomin');
+        return;
+    }
+  })
+);
+dots.forEach(dot =>
+  dot.addEventListener('mouseout', function () {
+    document.querySelectorAll('.dot > i').forEach(i => (i.style.display = 'none'));
+  })
+);
+
+// Handle Skills Carousel
+
+const root = document.documentElement;
+// Check how many skill ele
+// const skillsContent = document.querySelector('ul.skills-content');
+// root.style.setProperty('--skills-elements', skillsContent.children.length);
+
+for (let i = 0; i < 4; i++) {
+  // skillsContent.appendChild(skillsContent.children[i].cloneNode(true));
+}
+
+// Hanlde real time
+function concatZero(timeFrame) {
+  return timeFrame < 10 ? '0'.concat(timeFrame) : timeFrame;
+}
+
+function showTime() {
+  let today = new Date();
+  let day = today.getDay();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  let dayArr = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  document.getElementById('menuTime').innerHTML = `${dayArr[day]} ${concatZero(h)}:${concatZero(
+    m
+  )}:${concatZero(s)}`;
+  setInterval(() => {
+    showTime();
+  }, 1000);
+}
+showTime();
+
+// Control main content
